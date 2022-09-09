5705ed8c55e2ae2846e583ca64416f90ff23b53a
irvinzato <multizato@hotmail.com>
2022-09-08 22:57:06 -0500

    Inicie con fetchType, Lazy y Eager, primer ejemplo y espacio de trabajo

---
+++ src/main/java/org/rivera/hibernateapp/fetchtype/HibernateFetchLazyOneToMany.java
@@ -0,0 +1,19 @@
+package org.rivera.hibernateapp.fetchtype;
+
+import jakarta.persistence.EntityManager;
+import org.rivera.hibernateapp.entity.Cliente;
+import org.rivera.hibernateapp.util.JpaUtil;
+
+public class HibernateFetchLazyOneToMany {
+  public static void main(String[] args) {
+    System.out.println("Por defecto las anotaciones que terminan en 'Many' son Lazy y las que terminan en One son 'Eager'");
+    System.out.println("Lazy hace consultas solo cuando son utilizadas, 'Eager' siempre carga todo aunque no se utilice(significa entusiasta)");
+    System.out.println("Como son solo consultas no necesito el try catch");
+    EntityManager em = JpaUtil.getEntityManager();
+
+    Cliente client = em.find(Cliente.class, 1L);  //Hace solo una consulta, a menos que utilice algún "get" que involucre otra tabla
+    //System.out.println(client.getListAddress());
+
+    em.close();
+  }
+}
--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,3 +1,5 @@
 INSERT INTO clientes (id, nombre, apellido, forma_pago, creado_en, editado_en) VALUES (1,'Mauricio','Rivera','debito',NULL,NULL),(2,'Angeles','Lopez','credito',NULL,NULL),(3,'Jade','Aketza','paypal',NULL,NULL),(5,'Nombre modificado2','Apellido modificado','paypal',NULL,NULL),(6,'Mauricio','Rivera','debito',NULL,NULL),(7,'Marcos','Gomez','credito',NULL,NULL),(8,'Mauro','Rosales','paypal',NULL,NULL),(9,'Adan','Paraiso','debito',NULL,NULL),(10,'Lubu','Sanz','paypal',NULL,NULL),(11,'Luci','Perez','paypal',NULL,NULL),(12,'Luna','Guzman','credito',NULL,NULL),(13,'Irving','Banderley','paypal',NULL,NULL),(14,'Raiden','Acaros','paypal','2022-09-06 23:35:24','2022-09-06 23:38:50'),(15,'Sasaki','Coyiro','debito','2022-09-06 23:52:39','2022-09-06 23:55:03');
 INSERT INTO alumnos (id, nombre, apellido) VALUES (1,"Joana","Dimas"),(2,"Laura","Cervantes");
 INSERT INTO cursos (id, titulo, profesor) VALUES (1,"Docker","Ruben"),(2,"FlexBox","Dalto"),(3,"React","Teresa");
+INSERT INTO direcciones (calle, numero) VALUES ("Humores", 135),("Juarez", 23);
+INSERT INTO clientes_direcciones (id_cliente, id_direccion) VALUES (1,1),(1,2);
